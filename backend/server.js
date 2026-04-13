7b7e86050ceb0df2b67d75a02df02346e3e7f03c
Omkar Sathe <satheomkar71@gmail.com>
2026-04-13 22:27:11 +0530

    Task schema setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from '../backend/src/config/db.js'
+
+dotenv.config()
+
+const app = express();
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send("Hello... buddy'ss")
+})
+
+app.listen(4000, () => {
+    console.log("Server running on port 3000")
+})
+
+
+
---
+++ backend/src/models/Taskmodel.jsx
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'To do'
+    },
+    priority: {
+        type: String,
+        default: 'Medium'
+    }
+}, { timestamps: true });
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
+
+
+
