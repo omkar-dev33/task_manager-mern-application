5719c4b2e8730ba58631779a4dfc431fb356ced3
Omkar Sathe <satheomkar71@gmail.com>
2026-08-05 22:49:06 +0530

    build

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,53 @@ import cors from 'cors'
 import taskRoutes from './src/routes/taskRoutes.js'
 import connectDB from './src/config/db.js'
 import authRoute from './src/routes/authRoutes.js'
+import path from "path"
 
 dotenv.config()
 connectDB();
 
 const app = express();
+const __dirname = path.resolve()
+console.log(__dirname)
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(
+        cors({
+            origin:"http://localhost:5173",
+        })
+    );
+}
 app.use(express.json()); // convert request body to json
+
 
 // Routes
 app.use("/api/tasks", taskRoutes);
-
 app.use("/api/auth", authRoute);
+
+
+if(process.env.NODE_ENV === "production"){
+
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("/*splat",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+  
+}
+
+
+
+
+
+// app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+// app.get("*",(req,res)=>{
+//     res.send(path.join(__dirname,"../frontend","dist","index.html"));
+// })
 
 
 // Test route
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL:
+    import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
 });
 
 API.interceptors.request.use((req) => {
